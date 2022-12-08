54d75f8264688d07e73665e6ad93afb60c92171a
ThangMaiFE <m.c.quyetthang@gmail.com>
2022-12-08 20:47:11 +0700

    8/12

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,6 +1,11 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
   return (
-    <div>
+    <div className={cx('wrapper')}>
       <h1>Sidebar</h1>
     </div>
   );
